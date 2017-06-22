34a031f63bc92cf4480b12741de325cdee0af9d5
Ivo Georgiev <ivo@linvo.org>
2017-06-22 20:10:06 +0300

    add args to migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var AdExContrib = artifacts.require("./AdExContrib.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(AdExContrib);
+  deployer.deploy(AdExContrib, web3.eth.accounts[0], web3.eth.accounts[0],  web3.eth.accounts[0], Math.floor(Date.now()/1000), 0);
 };
